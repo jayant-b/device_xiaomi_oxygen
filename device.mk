d91d240b6f6d4f3073dca9d002b07ad64c9f362d
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    oxygen: Add libbt-vendor
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b
    (cherry picked from commit cc56485ea9588820aeb732b0474073667b1957a6)

--- device.mk
+++ device.mk
@@ -202,7 +202,8 @@ PRODUCT_PACKAGES += \
  
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor 
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
