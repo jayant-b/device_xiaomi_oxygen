d734d334580c8469d7fd3385a1f2b8537c9cd373
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    oxygen: Build Display HIDL HAL interfaces
     * Enable binderized Gralloc HAL along with it
    
    Change-Id: I61c34a531e3131209a08b5b4670e1f1f8fbb435d

--- device.mk
+++ device.mk
@@ -112,6 +112,11 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     copybit.msm8953 \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
