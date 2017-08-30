646add2c280357cfb909c9eec050bd6c7c7435a4
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-08-30 21:04:31 +0800

    mido: add seccomp policy
    Change-Id: I2dcc7b256044dcf8db997359b3c4f785daae4cab

--- device.mk
+++ device.mk
@@ -295,8 +295,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 
 # Sensors
 PRODUCT_COPY_FILES += \
