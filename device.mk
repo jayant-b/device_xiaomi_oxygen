c252e65d5140b0a0c1d728eda5914ddcef26586f
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-09 02:50:10 +0530

    oxygen : Build Keymaster HAL
    Change-Id: I8273f919c4a6d20c86245ca48145352dbd6eee53

--- device.mk
+++ device.mk
@@ -179,6 +179,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
