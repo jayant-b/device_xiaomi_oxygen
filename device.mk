b829939bc34c372ae5d78010725379efd17bf013
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-09 02:33:27 +0530

    oxygen: Build sensors HAL
    Change-Id: Ia43f43d30486129d1274a88f720880469d43b675

--- device.mk
+++ device.mk
@@ -241,6 +241,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
     sensors.msm8953
 
 # Thermal
