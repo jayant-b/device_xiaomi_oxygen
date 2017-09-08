7fb52df62bedf3f6db27d87300d4e7e6afc08d8f
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-09 02:54:03 +0530

    oxygen: Build bluetooth HAL
    Change-Id: I860da7df5e8e4d7930a86aac063572d03fe6c8c7

--- device.mk
+++ device.mk
@@ -179,6 +179,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
