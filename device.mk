eccdf9934f89e0cf562f87c8a60f207e0a2cbf44
Nikit Sharma <nikitsharma92@gmail.com>
2018-03-25 22:55:06 +0530

    oxygen: Add Seccomp policy

--- device.mk
+++ device.mk
@@ -271,6 +271,11 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     libxml2
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
