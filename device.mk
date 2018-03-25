e10987aa8a982b648a7d96d8073bb2a156738253
Nikit Sharma <nikitsharma92@gmail.com>
2018-03-25 22:24:25 +0530

    oxygen: Build DRM HAL impl

--- device.mk
+++ device.mk
@@ -135,6 +135,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprintd \
