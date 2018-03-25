04289cc1f2a3e720a5b04f00486a0f8c6cbb248e
Nikit Sharma <nikitsharma92@gmail.com>
2018-03-25 22:22:59 +0530

    oxygen: Build USB HAL service

--- device.mk
+++ device.mk
@@ -188,6 +188,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
