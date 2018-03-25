ecc101f42c45314e9bf6014a38349b9bcf28c2c8
Nikit Sharma <nikitsharma92@gmail.com>
2018-03-25 22:17:46 +0530

    oxygen: Build RenderScript HAL

--- device.mk
+++ device.mk
@@ -248,6 +248,10 @@ PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.post_boot.sh
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
