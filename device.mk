20af5f44f567cd4bb075372134d876acb9e306bb
yshalsager <ysh-alsager@hotmail.com>
2018-03-30 23:58:37 +0530

    RR-ify

--- device.mk
+++ device.mk
@@ -261,7 +261,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \ 
+    android.hardware.power@1.0-impl \
     power.msm8953
 
 # QMI
--- rr.mk
+++ rr.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/oxygen/full_oxygen.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_oxygen
+PRODUCT_NAME := rr_oxygen
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
